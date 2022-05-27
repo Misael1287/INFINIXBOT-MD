54f6ec1a4db1503a1c19879e0ee1318f160b09ae
MISAEL <105521930+Misael1287@users.noreply.github.com>
2022-05-27 13:03:28 -0500

    Update set-cmd.js

--- plugins/set-cmd.js
+++ plugins/set-cmd.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     db.data.sticker = db.data.sticker || {}
-    if (!m.quoted) throw `🔹Responde a un mensaje con *${usedPrefix + command}*`
-    if (!m.quoted.fileSha256) throw '⚠️ Falta el hash SHA256'
+    if (!m.quoted) throw `🔹 ʀᴇꜱᴘᴏɴᴅᴇ ᴀ ᴜɴ ꜱᴛɪᴄᴋᴇʀ ᴄᴏɴ ᴇʟ ᴄᴏᴍᴀɴᴅᴏ *${usedPrefix + command}*`
+    if (!m.quoted.fileSha256) throw '🔹 Falta el hash SHA256'
     if (!text) throw `🔹 Falta el comando`
     let sticker = db.data.sticker
     let hash = m.quoted.fileSha256.toString('base64')
-    if (sticker[hash] && sticker[hash].locked) throw '⚠️ No tienes permiso para cambiar este comando de Sticker'
+    if (sticker[hash] && sticker[hash].locked) throw '⚠️ ɴᴏ ᴛɪᴇɴᴇꜱ ᴘᴇʀᴍɪꜱᴏ ᴘᴀʀᴀ ᴄᴀᴍʙɪᴀʀ ᴇꜱᴛᴇ ᴄᴏᴍᴀɴᴅᴏ ᴅᴇ ꜱᴛɪᴄᴋᴇʀ'
     sticker[hash] = {
         text,
         mentionedJid: m.mentionedJid,
@@ -13,7 +13,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
         at: + new Date,
         locked: false,
     }
-    m.reply(`🔹 Comando guardado`)
+    m.reply(`🔹 ᴄᴏᴍᴀɴᴅᴏ ɢᴜᴀʀᴅᴀᴅᴏ`)
 }
 
 
