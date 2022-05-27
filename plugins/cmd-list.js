f41c554e97885350779ee1694f6cca899fff1a1e
MISAEL <105521930+Misael1287@users.noreply.github.com>
2022-05-27 13:07:00 -0500

    Create cmd-list.js

---
+++ plugins/cmd-list.js
@@ -0,0 +1,18 @@
+let handler = async (m, { conn }) => {
+    conn.reply(m.chat, `
+★━━━━━━━━━━━━━━━━━━━━★
+𝑳𝑰𝑺𝑻𝑨 𝑫𝑬 𝑪𝑶𝑴𝑨𝑵𝑫𝑶𝑺
+★━━━━━━━━━━━━━━━━━━━━★
+${Object.entries(global.db.data.sticker).map(([key, value], index) => `${index + 1}. ${value.locked ? `(bloqueado) ${key}` : key} : ${value.text}`).join('\n')}
+
+`.trim(), null, {
+        mentions: Object.values(global.db.data.sticker).map(x => x.mentionedJid).reduce((a, b) => [...a, ...b], [])
+    })
+}
+
+
+handler.help = ['listcmd']
+handler.tags = ['database']
+handler.command = ['listcmd']
+
+export default handler
