dddca7d41eb50df33180c958a2c2320c8b4ad15a
MISAEL <105521930+Misael1287@users.noreply.github.com>
2022-05-27 13:34:26 -0500

    Create tourl.js

---
+++ plugins/tourl.js
@@ -0,0 +1,22 @@
+import uploadFile from '../lib/uploadFile.js'
+import uploadImage from '../lib/uploadImage.js'
+
+let handler = async (m) => {
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
+  if (!mime) throw '🔹 ʀᴇꜱᴘᴏɴᴅᴇ ᴀ ᴜɴᴀ ɪᴍᴀɢᴇɴ/ᴠɪᴅᴇᴏ'
+  let media = await q.download()
+  let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
+  let link = await (isTele ? uploadImage : uploadFile)(media)
+  m.reply(`—————-ｷﾘﾄﾘｾﾝ—————-
+🔼  ${media.length} Byte(s) 
+🔼  ${isTele ? '(Sin fecha de caducidad)' : '(Desconocido)'} 
+🔼  *URL :* ${link}
+—————-ｷﾘﾄﾘｾﾝ—————-
+  `)
+}
+handler.help = ['tourl']
+handler.tags = ['herramientas']
+handler.command = ['upload', 'tourl']
+
+export default handler
