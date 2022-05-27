29c43d2ee18a38976cbd5a5bfff71988df15b9ff
MISAEL <105521930+Misael1287@users.noreply.github.com>
2022-05-27 09:14:23 -0500

    Update rank-peru.js

--- plugins/rank-peru.js
+++ plugins/rank-peru.js
@@ -33,7 +33,7 @@ ${author}
 
  conn.sendFile(m.chat, pp, './media/peruanos.jpg', lista, m, null, {
             contextInfo: {
-                mentionedJid: [a, b, c, d, e, f, g, h, i, j] [lista]
+                mentionedJid: [a, b, c, d, e, f, g, h, i, j]
             }
         })
 
