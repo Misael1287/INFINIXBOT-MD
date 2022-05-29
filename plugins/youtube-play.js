0559f6df908fbd297a0369ec9f2af457806fbe06
MISAEL <105521930+Misael1287@users.noreply.github.com>
2022-05-29 08:05:56 -0500

    Update youtube-play.js

--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -15,8 +15,8 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
   `.trim(), author, thumbnail, url, '📺Miralo en YouTube!', null, null, [
     ['Audio 🎧', `${usedPrefix}yta ${url} yes`],
     ['Video 🎥', `${usedPrefix}ytv ${url} yes`],
-    ['Buscador de Youtube🔎', `${usedPrefix}buscaryt ${url}`],
-    ['Play Doc 📄', `${usedPrefix}lllmd ${url}`]
+    ['Play Doc 📄', `${usedPrefix}lllmd ${url}`],
+    ['Buscador de Youtube🔎', `${usedPrefix}buscaryt ${url}`]
   ], m)
 }
 handler.help = ['ᴘʟᴀʏ <ᴛɪᴛᴜʟᴏ>','ᴘʟᴀʏ2']
