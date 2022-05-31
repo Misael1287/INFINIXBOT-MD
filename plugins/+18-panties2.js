1357511df45684fd4d62aab8d3313c55e256e561
MISAEL <105521930+Misael1287@users.noreply.github.com>
2022-05-31 08:11:42 -0500

    Update +18-panties2.js

--- plugins/+18-panties2.js
+++ plugins/+18-panties2.js
@@ -5,12 +5,12 @@ import fetch from 'node-fetch'
 if (!db.data.chats[m.chat].antojar && m.isGroup) throw '*‼Comandos +18 desactivados, ¿Deseas activarlos? si eres administrador escribe #enable antojar.*'         
     heum = await fetch(`https://api-reysekha.herokuapp.com/api/nsfw/panties?apikey=APIKEY`)
     json = await heum.buffer()
-   conn.sendButton(m.chat, '🔹ᴛᴏᴍᴀ ᴛᴜ ᴄᴏɴᴛᴇɴɪᴅᴏ ᴍᴀꜱᴛᴇʀ 🥵 ', author, json, [['SIGUIENTE ➡', `${usedPrefix}panties2`]], m)
+   conn.sendButton(m.chat, '🔹ᴛᴏᴍᴀ ᴛᴜ ᴄᴏɴᴛᴇɴɪᴅᴏ ᴍᴀꜱᴛᴇʀ 🥵 ', 'Click en siguiente para ir a la siguiente imagen', json, [['SIGUIENTE ➡', `${usedPrefix}panties2`]], m)
 }
 
 handler.tags = ['menu 18']
 handler.help = ['ᴘᴀɴᴛɪᴇꜱ2']
 
 handler.command = /^(panties2)$/i
-handler.limit = 5
+handler.limit = 1
 export default handler
