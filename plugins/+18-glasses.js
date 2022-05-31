55c7c1bea69019f2d3d0da4084b799e48fb49f00
MISAEL <105521930+Misael1287@users.noreply.github.com>
2022-05-31 08:07:32 -0500

    Update +18-glasses.js

--- plugins/+18-glasses.js
+++ plugins/+18-glasses.js
@@ -5,12 +5,12 @@ import fetch from 'node-fetch'
 if (!db.data.chats[m.chat].antojar && m.isGroup) throw '*‼Comandos +18 desactivados, ¿Deseas activarlos? si eres administrador escribe #enable antojar.*'        
     heum = await fetch(`https://api-reysekha.herokuapp.com/api/nsfw/glasses?apikey=APIKEY`)
     json = await heum.buffer()
-   conn.sendButton(m.chat, '🔹ᴛᴏᴍᴀ ᴛᴜ ᴄᴏɴᴛᴇɴɪᴅᴏ ᴍᴀꜱᴛᴇʀ 🥵 ', author, json, [['SIGUIENTE ➡', `${usedPrefix}glasses`]], m)
+   conn.sendButton(m.chat, '🔹ᴛᴏᴍᴀ ᴛᴜ ᴄᴏɴᴛᴇɴɪᴅᴏ ᴍᴀꜱᴛᴇʀ 🥵 ', 'Click en siguiente para ir a la siguiente imagen', json, [['SIGUIENTE ➡', `${usedPrefix}glasses`]], m)
 }
 
 handler.tags = ['menu 18']
 handler.help = ['ɢʟᴀꜱꜱᴇꜱ']
 
 handler.command = /^(glasses)$/i
-handler.limit = 3
+handler.limit = 1
 export default handler
