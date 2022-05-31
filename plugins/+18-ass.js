9b718cd4fae461bb1d37cced96eda74fc2e1a1ee
MISAEL <105521930+Misael1287@users.noreply.github.com>
2022-05-31 08:05:59 -0500

    Update +18-ass.js

--- plugins/+18-ass.js
+++ plugins/+18-ass.js
@@ -5,12 +5,12 @@ import fetch from 'node-fetch'
       if (!db.data.chats[m.chat].antojar && m.isGroup) throw '*‼Comandos +18 desactivados, ¿Deseas activarlos? si eres administrador escribe #enable antojar.*'    
     heum = await fetch(`https://api-reysekha.herokuapp.com/api/nsfw/ass?apikey=APIKEY`)
     json = await heum.buffer()
-   conn.sendButton(m.chat, '🔹ᴛᴏᴍᴀ ᴛᴜ ᴄᴏɴᴛᴇɴɪᴅᴏ ᴍᴀꜱᴛᴇʀ 🥵 ', author, json, [['SIGUIENTE ➡', `${usedPrefix}ass`]], m)
+   conn.sendButton(m.chat, '🔹ᴛᴏᴍᴀ ᴛᴜ ᴄᴏɴᴛᴇɴɪᴅᴏ ᴍᴀꜱᴛᴇʀ 🥵 ', 'Click en siguiente para ir a la siguiente imagen', json, [['SIGUIENTE ➡', `${usedPrefix}ass`]], m)
 }
 
 handler.tags = ['menu 18']
 handler.help = ['ᴀꜱꜱ']
 
 handler.command = /^(ass)$/i
-handler.limit = 3
+handler.limit = 1
 export default handler
